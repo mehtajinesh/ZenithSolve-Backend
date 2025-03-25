b6fa92eb9a403d933d20e51adf2932c419f775a4
Jinesh Mehta <mehtajineshs@gmail.com>
2025-03-25 00:11:50 -0500

    added cors back

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 import uvicorn
 from app.db.utils import init_db
 from app.routers import categories, problems
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 init_db()
 app.include_router(categories.router)
